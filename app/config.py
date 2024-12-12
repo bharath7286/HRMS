e251dd4015da5e85ac0096c2a00e7222b689f9df
Kamatham Bharath <kamathambharath72@gmail.com>
2024-12-12 13:51:48 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,22 +1,23 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
+load_dotenv()  # This will load the environment variables from the .env file
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
     AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
-    AWS_REGION = 'ap-south-1'
+    AWS_REGION = 'ap-south-1'  # This can also be set via the .env file
     USER_TABLE = 'F13_HRMS_Users'
     EMPLOYEE_TABLE = 'F13_HRMS_Employees'
     LEAVE_TABLE = 'F13_HRMS_Leaves'
     S3_BUCKET = 'f13-hrms-documents'
 
+    # Cognito Configuration
     COGNITO_USERPOOL_ID = os.environ.get('COGNITO_USERPOOL_ID')
     COGNITO_CLIENT_ID = os.environ.get('COGNITO_CLIENT_ID')
     COGNITO_CLIENT_SECRET = os.environ.get('COGNITO_CLIENT_SECRET')
     COGNITO_REGION = os.environ.get('COGNITO_REGION')
+
 
 
